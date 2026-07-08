ca8e6d338c143659b6c977f8640c76cfb3d3567e
robot-in <shortclub3@gmail.com>
2026-07-08 11:47:59 +0530

    Section1: Job Portal Application

---
+++ Section1/Section1/src/test/java/com/s2p/Section1/Section1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s2p.Section1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
